39ba009508c3d1a4a002e76c7c38c0dd9e2eb409
James Brosnahan <jb172@zips.uakron.edu>
2019-06-18 12:44:41 -0400

    Main accepts an input file as the second argument

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
+#include <exception>
 #include <fstream>
 
 #include "GameOfLife.hpp"
 
 using namespace std;
 
-int main(){
-    ifstream input("input.txt");
+int main(int argc, char*argv[]){
     GameOfLife game;
-    if (input.is_open()){
-        input >> game;
+    ifstream input;
+    string input_file = "input.txt";
+    
+    if(argc == 2){
+      // two arguments, open second argument as input file and output to std::cout
+      input_file = argv[1];
     }
-    for(int i = 1;i<=3;i++){
+    try{
+      input.open(input_file);
+      if (input.is_open()){
+        input >> game;
+      }
+      input.close();
+      for(int i = 1;i<=3;i++){
         cout << game << endl;
         game.move();
+      }
+    }catch(const ifstream::failure& e){
+      cerr << "File io error";
     }
+    
 }
